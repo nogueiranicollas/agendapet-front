af226a08de8bcf9efd3daa202f0a89396853e748
nicollas nogueira <nicollas.nogueira@nutrien.com>
2022-08-19 09:23:57 -0300

    inicio do Front end

---
+++ src/components/Banner/index.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className ="banner">
+            <img src="/imagens/Banner.png" alt="Banner Principal da Pagina" />
+        </ header>
+    )
+
+}
+
+export default Banner
---
+++ src/components/Formulario/index.js
@@ -0,0 +1,27 @@
+import './Formulario.css'
+import CampoTexto from '../CampoTexto'
+
+
+const Formulario = () => {
+  return(
+  <section className='Formulario'>
+    <form >
+        <h2>Agende agora o atendimento do seu pet! <img src='/imagens/patinha.png' height='20px' alt='Imagem da Patinha'></img></h2>
+      <CampoTexto 
+        label = 'Nome'
+        placeholder = 'Digite seu nome'
+      />
+      <CampoTexto 
+        label = 'Pet'
+        placeholder = 'Nome do seu Amiguinho'
+      />
+      <CampoTexto 
+        label = 'Serviço'
+        placeholder = 'Escolha seu serviço'
+      />
+    </form>
+  </section>
+  )
+}
+
+export default Formulario
